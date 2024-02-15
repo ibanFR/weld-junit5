3ec15f757726e399479a00e338830bfbbf2cb181
ibanFR <ibanfernandezrojo@msn.com>
2024-02-15 08:16:18 +0000

    SimpleTest coding kata
    Notes for a SimpleTest coding kata

---
+++ src/test/java/com/ibanfr/basic/SimpleTest.java
@@ -0,0 +1,17 @@
+package com.ibanfr.basic;
+
+public class SimpleTest {
+
+
+    // write a test to check if the bean writes in the standard output
+    // the string "hello!" when the method sayHello is called
+    // use the System.out.println() method to capture the output
+    // and the assertThat() method to check the output
+    // use the @Test and @DisplayName annotations to name the test
+    // and the @Inject annotation to inject the bean
+    // use the @WeldSetup annotation to setup the weld container
+    // and the WeldInitiator class to create the weld container
+    // use the EnableWeld annotation to enable the weld container
+
+
+}
